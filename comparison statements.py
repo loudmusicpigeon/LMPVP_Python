8cc7074d838ab1e71d9147f3686179a19b0d72c1
loudmusicpigeon <sciencepoppin@gmail.com>
2021-03-11 11:37:00 -0400

    comparison statements assignment 1

---
+++ comparison statements.py
@@ -0,0 +1,16 @@
+# create a test to print() True or False for x is equal to 13
+x = 9 + 4
+if x==13:
+    print('True')
+else:
+    print('False')
+
+# create a test to print True or False for 3 + 3 is greater than 2 + 4
+a = 3 + 3
+b = 2 + 4
+if a>b:
+    print('3 + 3 is greater than 2 + 4')
+elif b>a:
+    print('2 + 4 is greater than 3 + 3')
+else:
+    print('They are equal')
