a93f2dfb606d2b0ab11eadb3f148963d62824d88
loudmusicpigeon <sciencepoppin@gmail.com>
2021-03-11 12:21:10 -0400

    dictionary assignment 1

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+num = input('Enter a number from 1 to 12: ')
+month_conversions = {'1':'January',
+'2':'Febuary',
+'3':'March',
+'4':'April',
+'5':'May',
+'6':'June',
+'7':'July',
+'8':'August',
+'9':'September',
+'10':'October',
+'11':'November',
+'12':'Decenber'}
+q = month_conversions[num]
+print(q)
