c3350019f57d0b14e74e4dd363f19704f16fff9e
loudmusicpigeon <sciencepoppin@gmail.com>
2021-03-11 10:13:20 -0400

    input from user 1

---
+++ input from user.py
@@ -0,0 +1,11 @@
+# Create code to store input in student_name variable (when run type a name and press Enter)
+student_name = input("enter the student name: ")
+print("Hi " + student_name)
+
+# determine the type of the variable student_name using type()  function
+type(student_name)
+print("Type: ", type(student_name))
+
+# determine the type() of input below by entering
+print("enter a name or number")
+test_input = input()
