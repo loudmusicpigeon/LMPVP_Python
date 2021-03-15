8e98aef8f02283708c55b11f56e81dd6d436d7b7
loudmusicpigeon <sciencepoppin@gmail.com>
2021-03-15 08:29:32 -0400

    calculator 1 changes

--- Calculator 1.py
+++ Calculator 1.py
@@ -1,25 +1,25 @@
-num1 = input('Enter a number: ')
-num2 = input('Enter a number: ')
+num1 = float(input('Enter a number: '))
+num2 = float(input('Enter a number: '))
 op = input('Enter an operation (+ - * /). ')
 if op=='+':
-    result = float(num1) + float(num2)
+    result = (num1) + (num2)
     print(num1, '+', num2, '=', result)
 elif op=='-':
     if num1>num2:
-        result = float(num1) - float(num2)
+        result = (num1) - (num2)
         print(num1, '-', num2, '=', result)
     else:
-        result = float(num2) - float(num1)
+        result = (num2) - (num1)
         print(num2, '-', num1, '=', result)
 elif op=='*':
-    result = float(num1) * float(num2)
+    result = (num1) * (num2)
     print(num1, '×', num2, '=', result)
 elif op=='/':
     if num1>num2:
-        result = float(num1) / float(num2)
+        result = (num1) / (num2)
         print(num1, '÷', num2, '=', result)
     else:
-        result = float(num2) / float(num1)
+        result = (num2) / (num1)
         print(num2, '÷', num1, '=', result)
 else:
     print('Invalid operation')
