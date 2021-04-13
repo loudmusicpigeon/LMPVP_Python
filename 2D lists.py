a3e32f8412ce8c08aedd5f241010ea377ef0e9a6
loudmusicpigeon <sciencepoppin@gmail.com>
2021-04-13 18:40:17 -0400

    2D lists assignment

--- 2D lists.py
+++ 2D lists.py
@@ -1,11 +1,12 @@
-colu = float(input('Enter a column number: '))
+colu = int(input('Enter a column number: '))
 number_grid = [
     [1,2,3],
     [4,5,6],
     [7,8,9],
     [0]
 ]
-if colu == 0:
-    for row in number_grid:
-        print(number_grid[0][0])
-    
+for col in number_grid:
+    try:
+        print(col[colu])
+    except:
+        print("invalid number")
